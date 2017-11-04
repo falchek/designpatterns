925ceee4f4c8ed079972aac9b3a8f7c91b4b8523
falchek <falchek@gmail.com>
2017-11-04 19:16:50 -0400

    Observer completed

---
+++ DesignPatterns/src/observer/weather/StatisticsDisplay.java
@@ -0,0 +1,35 @@
+package observer.weather;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+	private float maxTemp = 0.0f;
+	private float minTemp = 200;
+	private float tempSum= 0.0f;
+	private Subject weatherData;
+	private int numReadings;
+
+	public StatisticsDisplay(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+
+	public void update (float temperature, float pressure, float humidity){
+		float temp = temperature;
+		tempSum += temp;
+		numReadings++;
+
+		if (temp > maxTemp) {
+			maxTemp = temp;
+		}
+ 
+		if (temp < minTemp) {
+			minTemp = temp;
+		}
+
+		display();
+	}
+
+	public void display() {
+		System.out.println("Avg/Max/Min temperature = " + (tempSum / numReadings)
+			+ "/" + maxTemp + "/" + minTemp);
+	}
+}
