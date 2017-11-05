ccc01304666c15584401db70f49cf212c3541530
falchek <falchek@gmail.com>
2017-11-04 20:31:14 -0400

    Meeting example!

--- DesignPatterns/src/observer/weather/WeatherStation.java
+++ DesignPatterns/src/observer/weather/WeatherStation.java
@@ -9,7 +9,6 @@ public class WeatherStation {
 		StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
 		HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 		
-		
 		weatherData.setMeasurements(80, 65, 30.4f);
 		System.out.println();
 		weatherData.setMeasurements(82, 70, 29.2f);
